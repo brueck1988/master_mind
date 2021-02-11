a4a0b88d853518a369db8887226687d67783bbfa
Alexander Brueck <brueck1988@gmail.com>
2021-02-11 16:53:06 -0700

    Secret code working but unsure how to test

--- lib/secret_code.rb
+++ lib/secret_code.rb
@@ -1,45 +1,53 @@
 class SecretCodeGenerator
-  attr_reader :secret_code,
-              :red_pins,
-              :player_code,
-              :white_pins
 
-def initialize
-  @secret_code = ["Y", "G", "B", "B"]
-  @player_code = ["R", "G", "B", "Y"]
+   attr_reader :player_code,
+               :red_pin_count,
+               :white_pin_count
+                #:secret_code,
+
+
+ def initialize(player_code)
+#   #@secret_code = secret_code
+   @player_code = player_code
   @red_pins = Array.new(4)
   @white_pins = Array.new(4)
-end
-
+ end
+#
  def generate_code
 #   #color_options = ["R", "G", "B", "Y"]
-#   @secret_code = ["R", "G", "Y", "B"]#.sample(2) + ["R", "G", "B", "Y"].sample(2)
-#   # 4.times do |counter|
-#   #    rand = ["R", "G", "B", "Y"].sample(random: SecureRandom)
-#   #    secret_code += rand
-#   #     end
-#   #  p secret_code
+  #@secret_code = ["R", "G", "Y", "B"]
+   @secret_code = ["R", "G", "Y", "B"].sample(2) + ["R", "G", "B", "Y"].sample(2)
    end
 
-   def code_comparer
+   def code_comparer(player_code)
     4.times do |count1|
-      if @secret_code[count1] == @player_code[count1]
+      if @secret_code[count1] == player_code[count1]
         @red_pins[count1] = "red pin"
+        #require "pry";binding.pry
         end
          4.times do |count2|
-          if @secret_code[count2] == @player_code[count1]
+          if @secret_code[count2] == player_code[count1]
             @white_pins[count1] = "white pin"
           else
           end
         end
+
         p @red_pins
         p @white_pins
         p " "
 
      end
-
+     #require "pry";binding.pry
 
       #@pin_count
       #require "pry";binding.pry
    end
- end
+
+   def red_pin_count
+     @red_pin_count = @red_pins.count("red pin")
+   end
+
+   def white_pin_count
+     @white_pin_count = @white_pins.count("white pin") - @red_pins.count("red pin")
+   end
+end
--- test/secret_code_test.rb
+++ test/secret_code_test.rb
@@ -4,29 +4,52 @@ require './lib/secret_code'
 
 class SecretCodeGeneratorTest < Minitest::Test
   def test_it_exists
-    generated_code = SecretCodeGenerator.new
+    player_code = ["R", "G", "Y", "B"]
+    generated_code = SecretCodeGenerator.new(player_code)
     assert_instance_of SecretCodeGenerator, generated_code
   end
 
-  # def test_it_has_attributes
-  #   generated_code = SecretCodeGenerator.new
-  #   assert_equal [], generated_code.secret_code
-  # end
+  def test_it_has_attributes
+    player_code = ["R", "G", "Y", "B"]
+    generated_code = SecretCodeGenerator.new(player_code)
+    assert_equal player_code, generated_code.player_code
+  end
 
-  # def test_generate_code
-  #
-  #   generated_code = SecretCodeGenerator.new
-  #
-  #   assert_equal 4, generated_code.generate_code.length
-  # end
+  def test_generate_code
+    player_code = ["R", "G", "Y", "B"]
+    generated_code = SecretCodeGenerator.new(player_code)
+    generated_code.generate_code
+    assert_equal 4, generated_code.generate_code.length
+  end
 
   def test_code_comparer
-    generated_code = SecretCodeGenerator.new
-    generated_code.generate_code
-    generated_code.code_comparer
+    player_code = ["R", "G", "Y", "B"]
+    generated_code = SecretCodeGenerator.new(player_code)
+    secret_code = generated_code.generate_code
+
+    generated_code.code_comparer(player_code)
+
+    assert_equal 4, generated_code.code_comparer(player_code)
+  end
+
+  def test_red_pin_count
+    player_code = ["R", "G", "Y", "B"]
+    generated_code = SecretCodeGenerator.new(player_code)
+    #generated_code.generate_code
+    @secret_code = generated_code.generate_code
+    generated_code.code_comparer(player_code)
+    count = generated_code.red_pin_count
     #require "pry";binding.pry
+    assert_equal count , generated_code.red_pin_count
+  end
 
+  def test_white_pin_count
+    player_code = ["R", "G", "Y", "B"]
+    generated_code = SecretCodeGenerator.new(player_code)
+    #generated_code.generate_code
+    count = generated_code.code_comparer(player_code).count("red pin")
+    generated_code.code_comparer(player_code)
 
-    assert_equal "no pin", generated_code.code_comparer
+    assert_equal count, generated_code.white_pin_count
   end
 end
