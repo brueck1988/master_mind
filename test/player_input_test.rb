2b26b6cf87eed5f3f0c8b3acaf35127f9bc452bf
Olivia Charles <ocharles92@gmail.com>
2021-02-12 19:30:05 -0500

    Finished writing tests for player input

--- test/player_input_test.rb
+++ test/player_input_test.rb
@@ -2,6 +2,8 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/player_input'
 
+
+
 class PlayerInputTest < Minitest::Test
   def test_it_exists_and_has_attributes
     player_code = PlayerInput.new("rygb")
@@ -13,24 +15,38 @@ class PlayerInputTest < Minitest::Test
     player_code1 = PlayerInput.new("RYGB")
     assert_equal true, player_code1.valid_elements?
 
-    player_code2 = PlayerInput.new("pygb")
+    player_code2 = PlayerInput.new("pyb")
     assert_equal false, player_code2.valid_elements?
 
-    player_code3 = PlayerInput.new("GGGP")
+    player_code3 = PlayerInput.new("GGGPY")
     assert_equal false, player_code3.valid_elements?
   end
 
-  def test_input_is_length_of_four
+  def test_it_has_valid_length_or_invalid
+    # skip
     player_code1 = PlayerInput.new("RYGB")
     assert_equal true, player_code1.valid_length?
 
-    player_code2 = PlayerInput.new("pyg")
+    player_code2 = PlayerInput.new("pyb")
     assert_equal false, player_code2.valid_length?
 
-    player_code3 = PlayerInput.new("GGGGG")
+    player_code3 = PlayerInput.new("GGGPY")
     assert_equal false, player_code3.valid_length?
+  end
 
-    player_code4 = PlayerInput.new("rygb")
-    assert_equal true, player_code4.valid_length?
+  def test_it_has_valid_input
+    player_code1 = PlayerInput.new("rygb")
+    assert_equal true, player_code1.valid_input
+
+    player_code2 = PlayerInput.new("yyb")
+    assert_equal false, player_code2.valid_input
+
+    player_code3 = PlayerInput.new("GGGBY")
+    assert_equal false, player_code3.valid_input
+
+    player_code4 = PlayerInput.new("PGGBY")
+    assert_equal false, player_code4.valid_input
+
+
   end
 end
