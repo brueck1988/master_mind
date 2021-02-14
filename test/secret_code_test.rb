3019071f165e711f9f5cf4f06cb9e05ad93b4b5e
Alexander Brueck <brueck1988@gmail.com>
2021-02-14 13:20:05 -0700

    Formatting fixes

--- test/secret_code_test.rb
+++ test/secret_code_test.rb
@@ -14,18 +14,3 @@ class SecretCodeTest < Minitest::Test
     assert cpu_code.generate_code
   end
 end
-
-# @player_input == "q"
-# @message.quit
-#   exit
-# else @secret_code == @player_input
-#   finish_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-#   run_time =  (finish_time - @starting_time).to_i
-#   p "Congratulations! You have have cracked the code #{@player_input.input.join} in #{run_time} seconds using #{@count} guess(es)!!!"
-#   p "Would you like to play again? Press (P) to play again or (Q) to quit."
-#   user_prompt = gets.chomp.upcase
-# if @player_input == "P"
-#   start
-# elsif @player_input == "Q"
-#   @message.quit
-#   end
