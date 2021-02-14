67dd86a59f399cae087862d3e60d6278d0404a0c
Olivia Charles <ocharles92@gmail.com>
2021-02-13 22:21:33 -0500

    Updated player input class to account for other charcaters used.

--- lib/player_input.rb
+++ lib/player_input.rb
@@ -5,30 +5,35 @@ class PlayerInput
 
   def initialize(input)
     @input   = input.upcase.split('')
+    @message = Messages.new
   end
 
   def valid_elements?
-    if input.include?("R"||"G"||"Y"||"B")
+    if element_include_rgyb
       true
-    else
+    else !element_include_rgyb
+      @message.invalid_element
       false
+    end
+  end
+
+  def element_include_rgyb
+    input.all? do |character|
+    character == "R"|| character == "G"||character =="Y"||(character ==("B"))
     end
   end
 
   def valid_length?
-    @message = Messages.new
     if input.count == 4
       true
-    elsif input.count < 4
-      p @message.input_is_too_short
-      false
-    else input.count > 4
-      p @message.input_is_too_long
-      false
+    elsif input.count > 4
+       @message.input_is_too_long
+    else input.count <  4
+       @message.input_is_too_short
     end
   end
 
-  def valid_input
+  def valid_input?
     if valid_elements? == true && valid_length? == true
       true
     else
